0976b4e9994a74650ee5b3d8e8cfe0dddb933dc9
buhuan <535051216@qq.com>
2013-09-06 09:38:04 +0800

    1111

--- Conf/db.inc.php
+++ Conf/db.inc.php
@@ -8,7 +8,7 @@
  */
 
 //账号数据库,分发数据库
-
+/*
 define('DB_HOST','192.168.20.229');
 define('DB_NAME','mmo2d_admin');
 define('DB_USER','root');
@@ -16,7 +16,7 @@ define('DB_PWD','1234');
 define('DB_TYPE','Mysql');
 define('DB_PORT','3306');
 define('DB_PREFIX','ljzm_');
-
+*/
 /*
 define('DB_HOST','1.4.18.86');
 define('DB_NAME','mmo2d_admin');
@@ -25,14 +25,14 @@ define('DB_PWD','zhs0071231231!@#');
 define('DB_TYPE','Mysql');
 define('DB_PORT','1025');
 define('DB_PREFIX','ljzm_');*/
-/*
+
 define('DB_HOST','127.0.0.1');
 define('DB_NAME','mmo2d_admin');
 define('DB_USER','root');
 define('DB_PWD','');
 define('DB_TYPE','Mysql');
 define('DB_PORT','3306');
-define('DB_PREFIX','ljzm_');*/
+define('DB_PREFIX','ljzm_');
 
 //define('MEMCACHED_HOST','115.238.101.156');
 define('MEMCACHED_HOST','192.168.20.229');//内部测试
